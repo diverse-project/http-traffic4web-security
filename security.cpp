9d1cc0bd56924b0f0858c0aadd5fb4fd7c3355b3
Hissane <mouna.hissane@um5s.net.ma>
2021-10-19 15:59:32 +0200

    update security

--- security.cpp
+++ security.cpp
@@ -10,29 +10,19 @@ json model;
 int main(){
 	freopen ("mfile.txt","r",stdin);
 	cin >> model; 
-	
+	int flag = 0;
 	for(auto x : model["paths"]){
-		//cout << "x.key() is " << x.key() << endl; // path
 		for(auto y : x){
-			cout << "y is this : " << y << endl;
 			for(auto z : y.items()){
-				cout << "whearas z is : " << z << endl;
-				cout << "whearas z's key is : " << z.key() << endl;
-				cout << "whearas z's value is : " << z.value() << endl;
 				json object = z.value();
-				cout << "object is : " << object << endl; 
 				for(auto code : object.items()){
 					if (code.key() == "200"){
-						cout << "works";
+						flag = 1;
 					}
-					cout << "code is : " << code << endl;
-					cout << "code.key() is : " << code.key() << endl;
+					
 				}
-				if(z.key() == "responses"){
-					/*for(auto code : z.items()){
-						cout << "code is : " << code << endl;
-						cout << "code.key() is : " << code.key() << endl;
-					}*/
+				if(!flag){
+					cout << "status code 200 not defined "<< x.key()<<  endl;
 				}
 			}
 		}
