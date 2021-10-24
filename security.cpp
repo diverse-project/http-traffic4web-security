7cbf34f9d115f3123d61b3d3f2c1ac0434279b95
Hissane <mouna.hissane@um5s.net.ma>
2021-10-24 18:05:18 +0200

    add BOLA

--- security.cpp
+++ security.cpp
@@ -6,9 +6,11 @@ using namespace std;
 using json = nlohmann::json;
 
 json model;
-
+isNumber(const std::string &str) {    
+  return !str.empty() && str.find_first_not_of("0123456789") == string::npos;
+}
 int main(){
-	freopen ("VAmPI.txt","r",stdin);
+	freopen ("mfile.txt","r",stdin);
 	cin >> model; 
 	for (json::iterator it = model.begin(); it != model.end(); ++it) {
         if(it.key() == "paths"){
@@ -45,21 +47,64 @@ int main(){
 	}
 
 	cout << "#############################" << endl;
-	map<string,string> adjmat;
-	vector<vector<string>> adjlist;
-	set<string> names;
+
+	set<string> pathsset;
 	for (json::iterator it = model.begin(); it != model.end(); ++it) {
-        if(it.key() == "paths"){
-        	json model1 = it.value();
-        	for (json::iterator it1 = model1.begin(); it1 != model1.end(); ++it1) {
-			        cout << it1.key() << endl;
-			        string path = it1.key();
-
-
-			}
-
-        }
+	    if(it.key() == "paths"){
+	        json model1 = it.value();
+	        for (json::iterator it1 = model1.begin(); it1 != model1.end(); ++it1) {
+	                pathsset.insert(it1.key());
+	        }
+	    }
 	}
+	map<string,vector<string>> index;
+	for(auto xx: pathset){
+		auto rr = xx.end();
+		while(--rr != xx.begin()){
+			if(*rr == '/'){
+				string right = xx.substr(xx.begin();rr);
+				string left = xx.substr(rr+1;xx.end());
+				if(isNumber(right))	index[left].push_back(right);
+
+			} 
+		}
+	}
+	for(auto path : index){
+		cout << YELLOW << "[!] WARNING Possibility of ID enumeration on \"" << path.first << "\"" << RESET << endl;
+	}
+
+
+
+	/*
+	vector<vector<int>> foldertree;
+	int id = 0;
+	map<string,int> roots;
+	map<int,map<string,int>> index;
+	map<int,string> foldername;
+	for(string path : pathsset){
+	    auto folderarray = split(path,"/");
+	    assert(folderarray.size()>0);
+	    int &rootid = roots[folderarray[0]];
+	    if(rootid == 0) { // if new root take new id; roots are multiples of 1e7
+	        id = 10000000 + id - id%10000000;
+	        rootid = id;
+	        foldername[rootid] = folderarray[0];
+	    }
+	    int parid = rootid;
+	    for(int i = 1; i < foldersarray.size();i++){
+	        int &nodeid = index[parid][foldersarray[i]];
+	        if(nodeid !=0 ){
+	            parid = nodeid;
+	            continue;
+	        }
+	        if(nodeid == 0) nodeid = ++id;
+	        foldertree[parid].push_back(nodeid);
+	        foldername[nodeid] = folderarray[i];
+	        parid = nodeid;
+		    }
+		}*/
+
+
 
 	return 0;
 }
